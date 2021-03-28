c0e3e8c4e12031601c9a5efe8bc6ed9341a8d965
Manuinder Sekhon <manuinder@geekyants.com>
2021-03-28 21:35:00 +0530

    add more functions to c library

--- libc_deepspeech/libc_deepspeech.c
+++ libc_deepspeech/libc_deepspeech.c
@@ -19,3 +19,42 @@ void deepspeech_free_str(char *string)
         DS_FreeString(string);
     }
 }
+
+void *create_model(char *model_path)
+{
+    if (model_path == NULL)
+    {
+        return NULL;
+    }
+
+    ModelState *ctx;
+    int status = DS_CreateModel(model_path, &ctx);
+
+    return (void *)ctx;
+}
+
+void free_model(void *model_state)
+{
+    if (model_state != NULL)
+    {
+        ModelState *ptr = (ModelState *)model_state;
+        DS_FreeModel(ptr);
+    }
+}
+
+uint64_t model_sample_rate(void *model_state)
+{
+    if (model_state == NULL)
+    {
+        return 0;
+    }
+
+    ModelState *ptr = (ModelState *)model_state;
+    int sample_rate = DS_GetModelSampleRate(ptr);
+    return sample_rate;
+}
+
+char *speech_to_text(void *model_state, char *buffer, uint64_t buffer_size)
+{
+    return NULL;
+}
