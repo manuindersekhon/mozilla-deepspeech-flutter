5699f91d967336fd2c8a0ed3c6089235eb20add1
Manuinder Sekhon <manuinder@geekyants.com>
2022-05-20 18:11:52 +0530

    add dart ffi bindings for scorer functions

--- deepspeech_flutter/lib/deepspeech_flutter.dart
+++ deepspeech_flutter/lib/deepspeech_flutter.dart
@@ -12,32 +12,32 @@ typedef NativeFreeModel = Void Function(Pointer);
 typedef FreeModel = void Function(Pointer);
 typedef NativeModelSampleRate = Uint64 Function(Pointer);
 typedef ModelSampleRate = int Function(Pointer);
-typedef NativeSpeechToText = Pointer<Utf8> Function(
-    Pointer, Pointer<Uint8>, Uint64);
+typedef NativeSpeechToText = Pointer<Utf8> Function(Pointer, Pointer<Uint8>, Uint64);
 typedef SpeechToText = Pointer<Utf8> Function(Pointer, Pointer<Uint8>, int);
+typedef NativeEnableScorer = Int32 Function(Pointer, Pointer<Utf8>);
+typedef EnableScorer = int Function(Pointer, Pointer<Utf8>);
+typedef NativeDisableScorer = Int32 Function(Pointer);
+typedef DisableScorer = int Function(Pointer);
+typedef NativeSetScorerAlphaBeta = Int32 Function(Pointer, Float, Float);
+typedef SetScorerAlphaBeta = int Function(Pointer, double, double);
 
 class DeepspeechFlutter {
   factory DeepspeechFlutter() => _instance;
   static final DeepspeechFlutter _instance = DeepspeechFlutter._internal();
 
   DeepspeechFlutter._internal() {
-    _deepspeech = Platform.isAndroid
-        ? DynamicLibrary.open("libdeepspeechlibc.so")
-        : DynamicLibrary.process();
+    _deepspeech = Platform.isAndroid ? DynamicLibrary.open("libdeepspeechlibc.so") : DynamicLibrary.process();
 
-    _dsVersion =
-        _deepspeech.lookupFunction<DSVersion, DSVersion>('deepspeech_verison');
-    _dsFreeStr = _deepspeech
-        .lookupFunction<DSNativeFreeStr, DSFreeStr>('deepspeech_free_str');
-    _dsCreateModel =
-        _deepspeech.lookupFunction<CreateModel, CreateModel>('create_model');
-    _dsFreeModel =
-        _deepspeech.lookupFunction<NativeFreeModel, FreeModel>('free_model');
-    _dsModelSampleRate =
-        _deepspeech.lookupFunction<NativeModelSampleRate, ModelSampleRate>(
-            'model_sample_rate');
-    _dsSpeechToText = _deepspeech
-        .lookupFunction<NativeSpeechToText, SpeechToText>('speech_to_text');
+    _dsVersion = _deepspeech.lookupFunction<DSVersion, DSVersion>('deepspeech_verison');
+    _dsFreeStr = _deepspeech.lookupFunction<DSNativeFreeStr, DSFreeStr>('deepspeech_free_str');
+    _dsCreateModel = _deepspeech.lookupFunction<CreateModel, CreateModel>('create_model');
+    _dsFreeModel = _deepspeech.lookupFunction<NativeFreeModel, FreeModel>('free_model');
+    _dsModelSampleRate = _deepspeech.lookupFunction<NativeModelSampleRate, ModelSampleRate>('model_sample_rate');
+    _dsSpeechToText = _deepspeech.lookupFunction<NativeSpeechToText, SpeechToText>('speech_to_text');
+    _dsEnableScorer = _deepspeech.lookupFunction<NativeEnableScorer, EnableScorer>('enable_external_scorer');
+    _dsDisableScorer = _deepspeech.lookupFunction<NativeDisableScorer, DisableScorer>('disable_external_scorer');
+    _dsSetScorerAlphaBeta =
+        _deepspeech.lookupFunction<NativeSetScorerAlphaBeta, SetScorerAlphaBeta>('set_scorer_alpha_beta');
   }
 
   late final DynamicLibrary _deepspeech;
@@ -49,6 +49,9 @@ class DeepspeechFlutter {
   late final FreeModel _dsFreeModel;
   late final ModelSampleRate _dsModelSampleRate;
   late final SpeechToText _dsSpeechToText;
+  late final EnableScorer _dsEnableScorer;
+  late final DisableScorer _dsDisableScorer;
+  late final SetScorerAlphaBeta _dsSetScorerAlphaBeta;
 
   // Pointer to loaded model state
   Pointer? _modelCtxPointer;
@@ -81,10 +84,37 @@ class DeepspeechFlutter {
       samplePointer.elementAt(index).value = samples[index];
     }
 
-    Pointer<Utf8> _result =
-        _dsSpeechToText(_modelCtxPointer!, samplePointer, samples.length);
+    Pointer<Utf8> _result = _dsSpeechToText(_modelCtxPointer!, samplePointer, samples.length);
     malloc.free(samplePointer);
 
     return _result.toDartString();
+  }
+
+  int enableExternalScorer(String scorerFilePath) {
+    Pointer<Utf8> _path = scorerFilePath.toNativeUtf8();
+    if (_modelCtxPointer == null || _modelCtxPointer == nullptr) {
+      return -1;
+    }
+
+    int statusCode = _dsEnableScorer(_modelCtxPointer!, _path);
+    return statusCode;
+  }
+
+  int disableExternalScorer() {
+    if (_modelCtxPointer == null || _modelCtxPointer == nullptr) {
+      return -1;
+    }
+
+    int statusCode = _dsDisableScorer(_modelCtxPointer!);
+    return statusCode;
+  }
+
+  int setScorerAlphaBeta(double alpha, double beta) {
+    if (_modelCtxPointer == null || _modelCtxPointer == nullptr) {
+      return -1;
+    }
+
+    int statusCode = _dsSetScorerAlphaBeta(_modelCtxPointer!, alpha, beta);
+    return statusCode;
   }
 }
